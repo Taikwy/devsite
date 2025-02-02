374141503dcc97ea6e263d6600f626337dac1b71
Taikwy <taikwy.twa@gmail.com>
2025-02-02 04:06:39 -0800

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function Footer(){
+
+    return(
+        <footer>
+            <a href="https://bsky.app/profile/taikwy.bsky.social" target="_blank">bluesky</a> | 
+            <a href="https://x.com/Taikwy" target="_blank">twitter</a> | 
+            <a href="https://taikwy.itch.io/" target="_blank">itch</a>
+        </footer>
+    )
+}
+
+export default Footer
